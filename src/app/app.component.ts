eb9847163eead1e076a395648520b769c1502c73
Jasmine Lee <jasmine.al1722@gmail.com>
2019-04-05 09:45:26 -0700

    add component info for today's date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
 
-@Component({
-  selector: 'date-pipe',
-  templateUrl: '['./index.html']'
-  <div>
-    <p>Today is {{today | date}}</p>
-    <p>Or if you prefer, {{today | date:'fullDate'}}</p>
-    <p>The time is {{today | date:'jmZ'}}</p>
-    <p>Finally the date and time is {{today | date:'short'}}</p>
-  </div>`
-})
+
+export class AppComponent {
+  today: string = 'Today';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
