3d66fe934bac8791694458012318846629366756
Jasmine Lee <jasmine.al1722@gmail.com>
2019-04-05 09:30:08 -0700

    component for date-pipe in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+
+@Component({
+  selector: 'date-pipe',
+  templateUrl: '['./index.html']'
+  <div>
+    <p>Today is {{today | date}}</p>
+    <p>Or if you prefer, {{today | date:'fullDate'}}</p>
+    <p>The time is {{today | date:'jmZ'}}</p>
+    <p>Finally the date and time is {{today | date:'short'}}</p>
+  </div>`
+})
