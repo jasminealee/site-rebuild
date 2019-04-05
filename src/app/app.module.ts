d8fae556abe2cb64bf36157a5a1db9f75a347720
Jasmine Lee <jasmine.al1722@gmail.com>
2019-04-05 15:45:43 -0700

    add back to attempting to get ngModules to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,26 +16,3 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-export class AppData {
-  constructor(
-    public name: String
-    <input type="text" class="form-control" id="name"
-    required
-
-    newMovieReview: Review;
-    reviews: Review[] = [
-      new Review('Endgame', ['the best movie ever!'])
-    ];
-    addReview(newTitle: string, newThoughts: string) {
-      const newThoughtsArray = newThoughts.split(', ')
-      this.newMovieReview =new Review(newTitle, newThoughtsArray);
-      this.reviews.push(this.newMovieReview);
-    }
-
-    editReview() {
-      alert("Edit");
-    }
-  }
-  )
-}
